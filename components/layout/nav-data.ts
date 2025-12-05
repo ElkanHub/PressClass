c342d5f8ea1cf370e655b46d7c064155dcb49fd0
Elkanah Donkor <elkanahdonkor@gmail.com>
2025-12-05 12:20:40 +0000

    masive rollback

--- components/layout/nav-data.ts
+++ components/layout/nav-data.ts
@@ -4,6 +4,6 @@ export const navItems = [
     { href: "/dashboard", label: "Dashboard", icon: Home },
     { href: "/generator", label: "Generator", icon: Zap },
     { href: "/notes", label: "Notes", icon: FileText },
-    { href: "/classes", label: "Classes", icon: BookOpen },
-    { href: "/settings", label: "Settings", icon: Settings },
+    // { href: "/classes", label: "Classes", icon: BookOpen },
+    // { href: "/settings", label: "Settings", icon: Settings },
 ];
