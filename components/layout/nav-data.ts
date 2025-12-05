998dddfd109f0937f1afb4e731a5ac97d4c4b569
Elkanah Donkor <elkanahdonkor@gmail.com>
2025-12-05 12:30:35 +0000

    commented empty pages

--- components/layout/nav-data.ts
+++ components/layout/nav-data.ts
@@ -3,6 +3,6 @@ import { Home, BookOpen, Settings, Zap } from "lucide-react";
 export const navItems = [
     { href: "/dashboard", label: "Dashboard", icon: Home },
     { href: "/generator", label: "Generator", icon: Zap },
-    { href: "/classes", label: "Classes", icon: BookOpen },
-    { href: "/settings", label: "Settings", icon: Settings },
+    // { href: "/classes", label: "Classes", icon: BookOpen },
+    // { href: "/settings", label: "Settings", icon: Settings },
 ];
